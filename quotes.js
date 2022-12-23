66c4a13105200a0bb3ea2509da70ee085fd0b446
Roman Paprotsky <roman@paprotsky.com>
2022-12-23 18:18:48 +0200

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,30 @@
+const defaultQuotes = [
+  {
+    author: "Paulo Coelho",
+    datetime: "",
+    gender: "m",
+    reference: "",
+    text: "Relax. You can’t save someone from themselves"
+  },
+  {
+    author: "@raydalio",
+    datetime: "",
+    gender: "m",
+    reference: "https://twitter.com/raydalio",
+    text: "Pain + Reflection = Progress"
+  },
+  {
+    author: "@growthclinicc",
+    datetime: "",
+    gender: "m",
+    reference: "https://twitter.com/growthclinicc",
+    text: "If you lack discipline, you'll become the big talking mediocre"
+  },
+  {
+    author: "Margaret Thatcher",
+    datetime: "",
+    gender: "f",
+    reference: "",
+    text: "No one would remember the Good Samaritan if he’d only had good intentions; he had money, too"
+  }
+]
